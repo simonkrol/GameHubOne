0581e688c16c3456331323c271b505132c989d48
simonkrol <simonkrol11@gmail.com>
2017-01-14 01:10:21 -0500

    Create a user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
-	attr_accessor :name, :address, :email, :password
+	before_save { self.email=email.downcase }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: { message: "Name is required" }
+	validates :password, presence: { message: "Password is required" }
+	validates :password, length: {minimum:7, message: "Password must be 7 or more characters" }
+	validates :email, uniqueness: { case_sensitive: false, message: "Email already in use" }
+ 	validates :email, format: { with: VALID_EMAIL_REGEX } message: "Invalid Email"
+ 	validates :password, confirmation: { message: "Passwords don't match" }
+ 	validates :password_confirmation, presence: { message: "Password confirmation is required" }
+
+ 	has_secure_password
+ 	cattr_accessor :encrypted_pass, :salt, :encrypted_pass_confirmation
+
 
 	def initialize(attributes = {})
 		@name = attributes[:name]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114032744) do
+ActiveRecord::Schema.define(version: 20170114060432) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170114032744) do
     t.string   "email"
     t.string   "password"
     t.string   "salt"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
