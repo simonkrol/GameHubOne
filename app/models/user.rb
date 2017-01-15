770b2409aae02a56dc0f10ae25f54c11f7d15e19
simonkrol <simonkrol11@gmail.com>
2017-01-14 19:36:21 -0500

    remove email format requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
 	before_save { self.email=email.downcase }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: { message: "Name is required" }
 	validates :password, presence: { message: "Password is required" }
 	validates :password, length: {minimum:7, message: "Password must be 7 or more characters" }
 	validates :email, uniqueness: { case_sensitive: false, message: "Email already in use" }
- 	validates :email, format: { with: VALID_EMAIL_REGEX } message: "Invalid Email"
  	validates :password, confirmation: { message: "Passwords don't match" }
  	validates :password_confirmation, presence: { message: "Password confirmation is required" }
 
