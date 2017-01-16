938e0a49be70e8a0301b0834eca807bce9b78b50
simonkrol <simonkrol11@gmail.com>
2017-01-15 19:35:32 -0500

    add mandatory account creation fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
+	#cattr_accessor :password, :password_confirmation
+	has_secure_password
 	before_save { self.email=email.downcase }
-	validates :name, presence: { message: "Name is required" }
-	validates :password, presence: { message: "Password is required" }
-	validates :password, length: {minimum:7, message: "Password must be 7 or more characters" }
-	validates :email, uniqueness: { case_sensitive: false, message: "Email already in use" }
- 	validates :password, confirmation: { message: "Passwords don't match" }
- 	validates :password_confirmation, presence: { message: "Password confirmation is required" }
+	validates :name, presence: { message: " is required" }
+	validates :password, length: {minimum:7, message: "must be 7 or more characters" }
+	validates :email, uniqueness: { case_sensitive: false, message: "already in use" }
+ 	validates :password_confirmation, presence: { message: "can't be blank" }
 
- 	#has_secure_password
- 	cattr_accessor :password, :password_confirmation
+ 	
+ 	#attr_accessor :password, :password_confirmation
 
 
 	# def initialize(attributes = {})
