cef688fbc02e2779f5d1af226adc5b0e4221c7e1
simonkrol <simonkrol11@gmail.com>
2017-01-18 00:15:56 -0500

    fix postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170114060432) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170114060432) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
