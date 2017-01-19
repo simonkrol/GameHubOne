3852f0be90c5a23dfceb4d574a607f61106e6aa2
simonkrol <simonkrol11@gmail.com>
2017-01-18 23:52:33 -0500

    add login functionality and navbar buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Login and redirect to home page
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
