632e2cf45592de2c8f09fca34540c22cd4198ada
simonkrol <simonkrol11@gmail.com>
2017-01-19 12:34:44 -0500

    add remember me functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 	validates :password, length: {minimum:7, message: "must be 7 or more characters" }
 	validates :email, uniqueness: { case_sensitive: false, message: "already in use" }
  	validates :password_confirmation, presence: { message: "can't be blank" }
-
+ 	attr_accessor :remember_token
  	
  	#attr_accessor :password, :password_confirmation
 
@@ -17,10 +17,28 @@ class User < ApplicationRecord
 	# 	@email = attributes[:email]
 	# 	@password = attributes[:password]
 	# end
+	class << self
+		def digest(string)
+			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+			BCrypt::Password.create(string, cost: cost)
+		end
 
-	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCRYPT::Engine.cost
-		BCrypt::Password.create(string, cost: cost)
+		def new_token
+			SecureRandom.urlsafe_base64
+		end
+	end
+	def remember 
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
 
