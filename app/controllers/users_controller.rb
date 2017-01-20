f9dbca8419a99f25ea39288501639744ba4d5fd8
simonkrol <simonkrol11@gmail.com>
2017-01-19 19:39:44 -0500

    fix global editing ability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
 	def index
 		@users=User.all
 	end
@@ -36,5 +38,18 @@ class UsersController < ApplicationController
 
 	private def user_params
       params.require(:user).permit(:name, :address, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+    	unless logged_in?
+    		flash[:danger] = "Please log in."
+    		redirect_to login_url
+    	end
+    end
+
+    def correct_user
+    	@user = User.find(params[:id])
+    	flash[:danger] = "Invalid page"
+    	redirect_to(home_url) unless current_user?(@user)
     end
 end
